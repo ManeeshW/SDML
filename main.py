516caf0c019188f34b9f457c71972f71b0be0e5b
ManeeshW <maneesh@gwu.edu>
2023-10-22 17:10:26 -0400

    image scroll

--- main.py
+++ main.py
@@ -18,10 +18,23 @@ img_No = 1
 #Input_ImgDir = ImgDir + "input_data/Test_{}/".format(Test_no)
 images = os.listdir(Input_ImgDir)
 No_imgs_in_folder = len(images)
-print("No. Images : ",No_imgs_in_folder)
 curr_img_no = 0
+
+# Create the function for the trackbar since its mandatory but we wont be using it so pass.
+def imageScroll(x):
+    global img_No, image, scaledImg, curr_img_no
+    img_No = cv2.getTrackbarPos('Img No', 'Window')
+    curr_img_no = img_No
+    image = cv2.imread(Input_ImgDir+ "{:06}.jpg".format(img_No))
+    scaledImg= image.copy()
+    cv2.imshow("Window",scaledImg) 
+    pass
+
+
 pc = np.array([])
 rc = np.array([])
+
+
 Pcs = np.array([])
 catID = 1
 
@@ -35,58 +48,45 @@ top_left_corner_scale=[]
 bottom_right_corner_scale=[]
 Mouse_Move = []
 
-#TK = np.zeros(PW_label.shape[0])
-#PW = PW_label.copy()
-
 maxScaleUp = 100
 scaleFactor = 0
 #windowName = "Resize Image"
 trackbarValue = "Scale"
 
 PH = 1 #prediction horizon
- 
+
+def markPoint(pc, image,  win = "Window", color = (120,255,0)):
+    for i in range(pc.shape[0]):
+       cv2.circle(image, (pc[i][0],pc[i][1]) , 1,color, -1)
+    scaledImg= image.copy()
+    cv2.imshow(win,scaledImg)
+    return scaledImg
+
 # function which will be called on mouse input
 def drawLine(action, x, y, flags, *userdata):
   # Referencing global variables 
-  global MMD, top_left_corner,top_left_corner_scale, bottom_right_corner,bottom_right_corner_scale, scaledImg, image, pc, rc,  curr_img_no, scaleFactor
+  global  scaledImg, image, pc, rc,  curr_img_no
   # Mark the top left corner when left mouse button is pressed
   scaleValue = cv2.getTrackbarPos('Scale', 'Window')
   scaleFactor = 1+ scaleValue/100.0
   
   #cv2.displayOverlay("Window", str(1.34445))
   cv2.displayStatusBar("Window", "Image No : {}      {}|{}".format(img_No,int(x/scaleFactor),int(y/scaleFactor))) 
-  #cv2.circle(scaledImg, (x,y), 10, (255,0,0), -1)
-  #cv2.imshow("Window",scaledImg)
-  #
-#  scaleImg = scaleImage(radius)
+
   if action == cv2.EVENT_LBUTTONDOWN:
-   top_left_corner = [(x,y)]
-   top_left_corner_scale = [(int(x/scaleFactor),int(y/scaleFactor))]
-   
    pc = cat(pc, [x,y])
    rc = np.array([])
-    #arr = np.append([arr,], axis=0)
-    # When left mouse button is released, mark bottom right corner
-  elif action == cv2.EVENT_LBUTTONUP:
-    # Draw the rectangle
-    cv2.circle(scaledImg, top_left_corner[0], 1, (2,255,0), -1)
-    cv2.circle(image, top_left_corner_scale[0], 1, (2,255,0), -1)
-    # cv2.rectangle(image, top_left_corner_scale[0], bottom_right_corner_scale[0], (0,255,0),2, 8)
-    # cv2.rectangle(scaledImg, top_left_corner[0], bottom_right_corner[0], (0,255,0),2, 8)
-    cv2.imshow("Window",scaledImg)
+   scaledImg = markPoint(pc, image, win = "Window")
 
   elif action == cv2.EVENT_MOUSEMOVE:
+     cv2.line(scaledImg, (x-3,y),(x-6,y), (255,20,200), 1)
+     cv2.line(scaledImg, (x+3,y),(x+6,y), (255,20,200), 1)
+     cv2.line(scaledImg, (x,y-3),(x,y-6), (255,20,200), 1)
+     cv2.line(scaledImg, (x,y+3),(x,y+6), (255,20,200), 1)
+
      cv2.circle(scaledImg, (x,y), 1, (255,20,200), -1)
      cv2.imshow("Window",scaledImg)
      scaledImg = cv2.resize(image, None, fx=scaleFactor, fy = scaleFactor, interpolation = cv2.INTER_LINEAR)
-
-  elif action == cv2.EVENT_MBUTTONDOWN:
-     MMD = np.array([x,y])
-
-  elif action == cv2.EVENT_MBUTTONUP:
-     for i in range(pc.shape[0]):
-       if pc[i][0] ==  MMD[0] and pc[i][1] ==  MMD[1]:
-         cv2.circle(image, (MMD[0],MMD[1]), 1, (255,255,0), -1)
 
   if curr_img_no != img_No:
      curr_img_no = img_No
@@ -112,9 +112,10 @@ def scaleImage(value=0):
     return scaledImg
     
 def Next(*args):
-    global img_No, image, scaledImg, pc, PH, rc
+    global img_No, image, scaledImg, pc, PH, rc, No_imgs_in_folder
     img_no = img_No+1
-    PH = PH+1
+    if img_No < No_imgs_in_folder:
+        PH = PH+1
     if img_no <= No_imgs_in_folder:
        img_No = img_no
     else:
@@ -122,15 +123,11 @@ def Next(*args):
 
     image = cv2.imread(Input_ImgDir+ "{:06}.jpg".format(img_No))
     scaledImg= image.copy()
-    imN = torch.ones(pc.shape[0]) * (img_no-1)
-   #  print(torch.ones(Pc.shape[0]))
-   #  print(torch.from_numpy(Pc).size())
-   #  print(torch.cat((imN.unsqueeze(1),torch.from_numpy(Pc)), dim=1))
-    p = torch.cat((imN.unsqueeze(1),torch.from_numpy(pc)), dim=1)
-    trackedImg(scaledImg, PH)
+
     pc = np.array([])
  
     cv2.setTrackbarPos('Scale', 'Window', 0)
+    cv2.setTrackbarPos('Img No', 'Window', img_No)
     cv2.imshow("Window",scaledImg)
 
 def ShowTracked(*args):
@@ -148,13 +145,16 @@ def trackedImg(im, No):
     return im, p[0][No-1]
 
 def Track(*args):
-    global img_No, image, scaledImg, pc, PH
+    global img_No, image, scaledImg, pc, PH, No_imgs_in_folder
     PH = 1
     imN = torch.ones(pc.shape[0]) * (PH-1)
     quaries = torch.cat((imN.unsqueeze(1),torch.from_numpy(pc)), dim=1)
     print(quaries.size())
     torch.save(quaries, 'q.pt')
-    subprocess.check_output(["python3 Tracker.py -S {} -N {}".format(img_No, img_No + 100)],shell=True)
+    if img_No+100 > No_imgs_in_folder:
+        subprocess.check_output(["python3 Tracker.py -S {} -N {}".format(img_No, No_imgs_in_folder)],shell=True)
+    else:
+       subprocess.check_output(["python3 Tracker.py -S {} -N {}".format(img_No, img_No + 100)],shell=True)
     print("Img : ", img_No, " - ", img_No + 100, "  Tracked")
 
 def Refresh(*args):
@@ -213,6 +213,7 @@ def Back(*args):
     pc = np.array([])
 
     cv2.setTrackbarPos('Scale', 'Window', 0)
+    cv2.setTrackbarPos('Img No', 'Window', img_No)
     cv2.imshow("Window",scaledImg) 
     
 def Save(*args):
@@ -242,10 +243,10 @@ def OpenImgLabel(*args):
     im = cv2.imread(Input_ImgDir+ "{:06}.jpg".format(img_No))
     im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
     
-    pc = est_pc(Hc, K, cat_id = catID)
+    box_pc = est_pc(Hc, K, cat_id = catID)
     catID = catID+1
     #plot_box(pc, clr_lines = "green", clr_corners = "red", clr_int_corners = "blue")
-    plot_box(pc, clr_lines = "orange", corners = False, int_corners = False, linewidth=1.3,  label = 'TNN-PnP Est. for Real imgs')
+    plot_box(box_pc, clr_lines = "orange", corners = False, int_corners = False, linewidth=1.3,  label = 'TNN-PnP Est. for Real imgs')
     imshow(im)
     plt.savefig(out_label+"{:06}.jpg".format(img_No))
     im = cv2.imread(out_label+"{:06}.jpg".format(img_No))
@@ -283,6 +284,7 @@ cv2.createButton("Save",Save,None,cv2.QT_PUSH_BUTTON,1)
 
 # Create trackbar and associate a callback function / create trackbars Named Radius with the range of 150 and starting position of 5.
 cv2.createTrackbar('Scale', 'Window', 0, 200, scaleIt) 
+cv2.createTrackbar('Img No', 'Window', 1, No_imgs_in_folder, imageScroll) 
 # Create trackbar and associate a callback function
 #cv2.createTrackbar(trackbarValue, windowName, scaleFactor, maxScaleUp, scaleImage)
 
