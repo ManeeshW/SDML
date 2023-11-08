902459e2ef6622fc1fdb080ec3c2538b535373f4
ManeeshW <maneesh@gwu.edu>
2023-11-08 00:28:17 -0500

    new tracked keys saving mtd

--- Tracker.py
+++ Tracker.py
@@ -8,16 +8,20 @@ from config.config import *
 parser = argparse.ArgumentParser(description="Synthetic image generation")
 
 parser.add_argument('-S', type=int, default=100,
+                        help='Number of the starting')
+
+parser.add_argument('-N', type=int, default=150,
                         help='Number of images want to be generated')
 
-parser.add_argument('-N', type=int, default=423,
-                        help='Number of images want to be generated')
+parser.add_argument('-T', type=int, default=300,
+                        help='Total images')
 
 args = parser.parse_args()
 N = args.N
 S = args.S
+T = args.T
 #N = 20
-def mode(S, N):
+def mode(S, N, T):
     video = read_images_from_path(Input_ImgDir, S, N)
     video = torch.from_numpy(video).permute(0, 3, 1, 2)[None].float()
     # queries = torch.tensor([
@@ -27,7 +31,7 @@ def mode(S, N):
     #     [30., 900., 200.]
     # ])
     # print(queries.size())
-    queries = torch.load('q.pt')
+    queries = torch.load('tracked/q.pt')
     model = CoTrackerPredictor(checkpoint='cotracker/ckpt/cotracker_stride_4_wind_8.pth')
 
     if torch.cuda.is_available():
@@ -36,7 +40,18 @@ def mode(S, N):
         queries = queries.cuda()
 
     pred_tracks, pred_visibility = model(video, queries=queries.unsqueeze(0))
-    print(pred_tracks.size())
+
     torch.save(pred_tracks, 'tracked/tracked.pt')
-    
-mode(S, N)
+
+    try:
+        trackedDict = torch.load('tracked/tracked_Dict.pt')
+    except:
+        trackedDict = {}
+        trackedDict.update({"img{}".format(i+1) : False for i in range(T)})
+
+    for i in range(pred_tracks.size()[1]):
+        trackedDict["img{}".format(S+i)] = pred_tracks[0,i,:,:].cpu().numpy().astype(np.int16)
+
+    torch.save(trackedDict,'tracked/tracked_Dict.pt')
+
+mode(S, N, T)
