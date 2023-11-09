a68acd3980d3e9759eeaa4e44f55cfcb262fb0d8
ManeeshW <maneesh@gwu.edu>
2023-11-09 01:22:31 -0500

    load dataset

---
+++ MLib/rename.py
@@ -0,0 +1,14 @@
+import os
+
+def rename(input_path, output_path):
+    files = os.listdir(input_path)
+    i = 1
+    n = 1
+    while i < len(files)+1:
+        try:
+            print('{:06d}'.format(i) + ".jpg")
+            os.rename(input_path + '{:d}'.format(n) + ".png", output_path + '{:06d}'.format(i) + ".jpg")
+            i += 1
+        except:
+            pass
+        n += 1
