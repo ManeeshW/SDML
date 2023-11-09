0778298162035629c6cdcf15c486ea513307499a
ManeeshW <maneesh@gwu.edu>
2023-11-09 00:57:43 -0500

    change automatic data storing files

--- MLib/config.py
+++ MLib/config.py
@@ -5,38 +5,73 @@ import numpy as np
 config = ConfigParser(interpolation=ExtendedInterpolation())
 config.read("config/config.ini")
 
-Input_ImgDir = config.get('Dir', 'INPUT_DIR')
-CamProp = config.get('Dir', 'CAMERA')
-outputF = config.get('Dir', 'OUT_DIR')
-Test_no = config.getint('Test', 'TEST_NO')
-K = np.loadtxt(CamProp+"Blender_camera/K.txt")
-outputFolder = outputF +"Test_{}/".format(Test_no)
-
-out_img = outputFolder+"imgs/"
-out_label = outputFolder+"label/"
-out_annot = outputFolder+"Annotations/"
-out_bimg = outputFolder+"bimg/"
-out_comp = outputFolder+"comparison/"
-out_keys = outputFolder+"keypoints/"
-track_dir = outputFolder+"tracked/"
+MAIN_DIR = config.get('Auto_Dir', 'MAIN_DIR')
+CamProp = config.get('Main_dir', 'CAMERA')
+MAIN_DIR_Data = config.get('Auto_Dir', 'MAIN_DIR_Data')
+MAIN_DIR_Labels = config.get('Auto_Dir', 'MAIN_DIR_Labels')
+COMBINED_DATA = config.get('Auto_Dir', 'COMBINED_DATA')
+TEST = config.get('Auto_Dir', 'TEST')
+TEST_L = config.get('Auto_Dir', 'TEST_L')
+REAL_IMG_DIR = config.get('Test', 'REAL_IMG_DIR')
+INPUT_IMG = config.get('Auto_Dir', 'INPUT_IMG_DIR')
 
 try:
-   os.mkdir(outputFolder)
+   os.mkdir(MAIN_DIR)
 except:
    pass
+
+try:
+   os.mkdir(MAIN_DIR_Data)
+except:
+   pass
+
+try:
+   os.mkdir(MAIN_DIR_Labels)
+except:
+   pass
+
+try:
+   os.mkdir(COMBINED_DATA)
+except:
+   pass
+
+try:
+   os.mkdir(TEST)
+except:
+   pass
+
+try:
+   os.mkdir(TEST_L)
+except:
+   pass
+
+try:
+   os.mkdir(INPUT_IMG)
+except:
+   pass
+
+
+#outputF = config.get('Dir', 'OUT_DIR')
+Test_no = config.getint('Test', 'TEST_NO')
+
+K = np.loadtxt(CamProp+"Blender_camera/K.txt")
+np.savetxt(TEST+"K.txt", K)
+#outputFolder = outputF +"Test_{}/".format(Test_no)
+#out_label = outputFolder+"label/"
+
+
+out_annot_test = TEST
+out_img = TEST_L+"imgs/"
+out_bimg = TEST_L+"imgs_blender/"
+out_comp = TEST_L+"imgs_comparison/"
+out_keys = TEST_L+"keypoints/"
+out_annot = TEST_L+"blender_annot/"
+track_dir = TEST_L+"tracked/"
+INPUT_IMG_DIR = config.get('Auto_Dir', 'INPUT_IMG_DIR')
+
 
 try:
    os.mkdir(out_img)
-except:
-   pass
-
-try:
-   os.mkdir(out_label)
-except:
-   pass
-
-try:
-   os.mkdir(out_annot)
 except:
    pass
 
@@ -52,6 +87,11 @@ except:
 
 try:
    os.mkdir(out_keys)
+except:
+   pass
+
+try:
+   os.mkdir(out_annot)
 except:
    pass
 
